fa5cf5074956f6f14c9ee3741c96d3faf405dfdb
kaankalttakiran <durdu.kaan.kaltakkiran@gmail.com>
2023-01-13 21:47:46 +0300

    Proje Finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+   /*  Container ayarları(varsayılan) */
+    container: {
+      center: true,
+       screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px', 
+      },
+    },
+    extend: {
+      /* Custom font kullanmak için extend yaptık */
+      fontFamily:{
+        gemunu:['Gemunu Libre', 'sans-serif'],
+        open:['Open Sans', 'sans-serif'],
+      },
+      /* Custom rekler */
+      colors: {
+        'gega-red': '#BC1A45',
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
+        'gega-white': '#F7F7F7',
+      },
+      spacing: {
+        128: '32rem',
+      },
+    }, 
   },
   plugins: [],
 }
